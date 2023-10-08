7f43f70e63551823ebe471feb7edd5c38e775a15
StiliyanIliev27 <stiliyanileiv2705@gmail.com>
2023-10-08 17:20:06 +0300

    Added tasks to Fundamentals Course

---
+++ 02.Passed.cs
@@ -0,0 +1,15 @@
+﻿namespace _02.Passed
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num = double.Parse(Console.ReadLine());
+
+            if (num >= 3.00)
+            {
+                Console.WriteLine("Passed!");
+            }
+        }
+    }
+}
