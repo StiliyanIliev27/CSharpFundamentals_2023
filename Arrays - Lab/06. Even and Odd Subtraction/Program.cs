0c6822d8b70d440472a9b912308f7390942aac59
StiliyanIliev27 <stiliyanileiv2705@gmail.com>
2023-11-02 23:28:21 +0200

    Added Arrays Lab tasks to Fundamentals C# Course

---
+++ Arrays - Lab/06. Even and Odd Subtraction/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _06._Even_and_Odd_Subtraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                 .Split().Select(int.Parse).ToArray();
+
+            int oddSum = 0;
+            int evenSum = 0;
+
+            int result = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    evenSum += numbers[i];
+                }
+                else if (numbers[i] % 2 != 0)
+                {
+                    oddSum += numbers[i];
+                }
+            }
+
+            if (true)
+            {
+                result = evenSum - oddSum;
+            }
+
+
+            Console.WriteLine(result);
+        }
+    }
+}
